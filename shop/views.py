85c8da2585521efcaf8d42a9190c623e2426817c
Мурзоев Мизбоншох Содирович <msmurzoev@edu.hse.ru>
2025-05-05 20:58:18 +0300

    Вторая версия ПО

--- shop/views.py
+++ shop/views.py
@@ -1,4 +1,5 @@
 from itertools import product
+from unicodedata import category
 
 from django.shortcuts import render, redirect
 from django.views.generic import ListView, DetailView
@@ -37,7 +38,28 @@ class SubCategories(ListView):
 
     def get_queryset(self):
         """Получение всех товаров подкатегории"""
+        if type_field := self.request.GET.get('type'):
+            products = Product.objects.filter(category__slug=type_field)
+            return products
+
         parent_category = Category.objects.get(slug=self.kwargs['slug'])
         subcategories = parent_category.subcategories.all()
         products = Product.objects.filter(category__in=subcategories).order_by('?')
+
+        if sort_field := self.request.GET.get('sort'):
+            products = products.order_by(sort_field)
+
+
+
+
+
         return products
+
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        """Дополнительные элементы"""
+        context = super().get_context_data()
+        parent_category = Category.objects.get(slug=self.kwargs['slug'])
+        context['category'] = parent_category
+        context['title'] = parent_category.title
+        return context
